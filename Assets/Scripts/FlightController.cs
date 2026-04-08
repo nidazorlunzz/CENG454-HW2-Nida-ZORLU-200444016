779542e149c59ed7848710a2e3420ba5186976ab
nidazorlunzz <s200444016@stu.thk.edu.tr>
2026-04-08 11:19:13 +0300

    change forward controller

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -1,52 +1,101 @@
 using UnityEngine;
 
-public class FlightController : MonoBehaviour
+public class AircraftController : MonoBehaviour
 {
-    public float speed;
-    public float maxSpeed = 400f;
-    public float minSpeed = 0f;
-    public float rotspeed1 = 30f;
-    public float rotspeed2 = 30f;
+    [SerializeField] private FlightExamController missionController;
 
-    void Update() 
+    [Header("Speed Settings")]
+    [SerializeField] private float currentSpeed = 0f;
+    [SerializeField] private float acceleration = 50f;
+    [SerializeField] private float maxVelocity = 500f;
+    [SerializeField] private float minVelocity = 0f;
+
+    [Header("Rotation Settings")]
+    [SerializeField] private float rollSpeed = 25f;
+    [SerializeField] private float pitchYawSpeed = 25f;
+
+    private bool takeoffTriggered = false;
+
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+        HandleExamControls();
+        CheckTakeoff();
+    }
+
+    private void HandleMovement()
     {
         
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position += -transform.forward * currentSpeed * Time.deltaTime;
 
-       
-        if(Input.GetKey(KeyCode.Space)){
-            if(speed < maxSpeed){
-                speed += 50f * Time.deltaTime;
-            }
-        }
-        if(Input.GetKey(KeyCode.LeftShift)){
-            if(speed > minSpeed){
-                speed -= 50f * Time.deltaTime;
-            }
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentSpeed = Mathf.Min(currentSpeed + acceleration * Time.deltaTime, maxVelocity);
         }
 
         
-        if(Input.GetKey(KeyCode.Q)){
-            transform.Rotate(Vector3.forward * rotspeed1 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = Mathf.Max(currentSpeed - acceleration * Time.deltaTime, minVelocity);
         }
-        if(Input.GetKey(KeyCode.E)){
-            transform.Rotate(Vector3.forward * -rotspeed1 * Time.deltaTime);
+    }
+
+    private void HandleRotation()
+    {
+        bool canRotate = currentSpeed >= 150f || transform.position.y >= 5f;
+
+        // roll
+        if (Input.GetKey(KeyCode.Q) && currentSpeed >= 150f)
+        {
+            transform.Rotate(Vector3.back * rollSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.E) && currentSpeed >= 150f)
+        {
+            transform.Rotate(Vector3.forward * rollSpeed * Time.deltaTime);
         }
 
         
-        if(Input.GetKey(KeyCode.W)){
-            transform.Rotate(Vector3.right * -rotspeed2 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W) && canRotate)
+        {
+            transform.Rotate(Vector3.left * pitchYawSpeed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.S)){
-            transform.Rotate(Vector3.right * rotspeed2 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S) && canRotate)
+        {
+            transform.Rotate(Vector3.right * pitchYawSpeed * Time.deltaTime);
         }
 
-       
-        if(Input.GetKey(KeyCode.A)){
-            transform.Rotate(Vector3.up * -rotspeed2 * Time.deltaTime);
+        
+        if (Input.GetKey(KeyCode.A) && canRotate)
+        {
+            transform.Rotate(Vector3.down * pitchYawSpeed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.D)){
-            transform.Rotate(Vector3.up * rotspeed2 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D) && canRotate)
+        {
+            transform.Rotate(Vector3.up * pitchYawSpeed * Time.deltaTime);
+        }
+    }
+
+    private void HandleExamControls()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            missionController.OnMissileHit();
+            missionController.ResetMission();
+
+            currentSpeed = 0f;
+            rollSpeed = 0f;
+            pitchYawSpeed = 0f;
+        }
+    }
+
+    private void CheckTakeoff()
+    {
+        if (!takeoffTriggered && transform.position.y >= 20f)
+        {
+            missionController.OnTakeoff();
+            takeoffTriggered = true;
         }
     }
 }
