9bc16ee2061ab6991f8f0cf99f123389b8fba18b
nidazorlunzz <s200444016@stu.thk.edu.tr>
2026-04-08 12:01:28 +0300

    change exam manager codes

--- Assets/Scripts/FlightExamManager.cs
+++ Assets/Scripts/FlightExamManager.cs
@@ -5,32 +5,35 @@ public class FlightExamManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text statusText;
 
+    private bool hasTakenOff = false;
     private bool inDangerZone = false;
+    private bool threatCleared = false;
 
+    
     public void EnterDangerZone()
     {
         inDangerZone = true;
         statusText.text = "Entered a Dangerous Zone!";
     }
 
+    
     public void ExitDangerZone()
     {
         inDangerZone = false;
+        threatCleared = true;
         statusText.text = "Safe Zone";
     }
 
+    
+    public void OnTakeoff()
+    {
+        hasTakenOff = true;
+        statusText.text = "Takeoff Successful";
+    }
+
+  
     public void OnMissileHit()
     {
         statusText.text = "You got hit!";
-    }
-
-    public void ResetMission()
-    {
-        statusText.text = "Mission Reset";
-    }
-
-    public void OnTakeoff()
-    {
-        statusText.text = "Takeoff successful";
     }
 }
